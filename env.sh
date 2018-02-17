7c88af806a8c21d4f67e62c4c0855278cd64873a
Adib Pratama <pratama.adib@gmail.com>
2018-02-17 14:59:58 +0700

    move environment variable to its own file, add transparance urxvt, and etc

---
+++ env.sh
@@ -0,0 +1,22 @@
+# for java
+# STUDIO_JDK=
+# export STUDIO_JDK
+
+# SCALA_HOME=
+# export SCALA_HOME
+
+# SPARK_HOME=/media/Linux/sys_local/standalone/spark-2.2.1-bin-hadoop2.7
+# export SPARK_HOME
+
+# PATH=$PATH:/home/adib/sys_local/bin:/home/adib/.local/bin
+# export PATH
+
+# for python disable write byte code
+PYTHONDONTWRITEBYTECODE=1
+export PYTHONDONTWRITEBYTECODE
+
+# for python library 
+# PYTHONPATH=$PYTHONPATH:/media/D/work:/media/D/work/nlp_pipeline
+# export PYTHONPATH
+
+
